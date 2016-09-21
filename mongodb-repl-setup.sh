9620b4884e9cba9c39ad4931bb1f6cdf663f8f55
dirkslab <dirkslab@gmail.com>
2016-09-21 16:21:30 +0200

    Add files via upload

--- mongodb-repl-setup.sh
+++ mongodb-repl-setup.sh
@@ -132,7 +132,7 @@ processManagement:
 net:
    port: 27017
 replication:
-   replSetName: "rs0"' > /etc/mongod.conf
+   replSetName: "rs1"' > /etc/mongod.conf
 
 # Create Mongo directories
 
@@ -306,6 +306,7 @@ Install_step7()
 yum install https://cloud.mongodb.com/download/agent/automation/mongodb-mms-automation-agent-manager-3.1.0.1831-1.x86_64.rhel7.rpm
 sed -i:bak '$ a\mmsGroupId= 513d91fd7fe227e9f1855cdb' /etc/mongodb-mms/automation-agent.config
 sed -i:bak '$ a\mmsGroupId= 88ca1ad3c453cb5bb8dff79ab9aecaeb' /etc/mongodb-mms/automation-agent.config
+systemctl start mongodb-mms-automation-agent.service
 }
 
 Install_step1
