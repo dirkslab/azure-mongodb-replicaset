c6e1ef8227f8b65c8e87308af2dd37992e0eea49
dirkslab <dirkslab@gmail.com>
2016-09-22 09:36:27 +0200

    Add files via upload

--- mongobackupsetup.sh
+++ mongobackupsetup.sh
@@ -1,3 +1,12 @@
 ﻿#!/bin/bash
+Install_step7()
+{
+yum install https://cloud.mongodb.com/download/agent/automation/mongodb-mms-automation-agent-manager-3.1.0.1831-1.x86_64.rhel7.rpm
+#sed -i:bak '$ a\mmsGroupId= 513d91fd7fe227e9f1855cdb' /etc/mongodb-mms/automation-agent.config
+#sed -i:bak '$ a\mmsGroupId= 88ca1ad3c453cb5bb8dff79ab9aecaeb' /etc/mongodb-mms/automation-agent.config
+systemctl start mongodb-mms-automation-agent.service
+}
+
+Install_step7
 
 exit
