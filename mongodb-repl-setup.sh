27007bf4be57394bdc0a8f80ebd2dc2d8533f8c7
dirkslab <dirkslab@gmail.com>
2016-09-22 09:28:20 +0200

    Add files via upload

--- mongodb-repl-setup.sh
+++ mongodb-repl-setup.sh
@@ -303,7 +303,7 @@ sed -i:bak '$ a\net.ipv4.tcp_keepalive_time = 120' /etc/yum.conf /etc/sysctl.con
 
 Install_step7()
 {
-#yum install https://cloud.mongodb.com/download/agent/automation/mongodb-mms-automation-agent-manager-3.1.0.1831-1.x86_64.rhel7.rpm
+yum install https://cloud.mongodb.com/download/agent/automation/mongodb-mms-automation-agent-manager-3.1.0.1831-1.x86_64.rhel7.rpm
 #sed -i:bak '$ a\mmsGroupId= 513d91fd7fe227e9f1855cdb' /etc/mongodb-mms/automation-agent.config
 #sed -i:bak '$ a\mmsGroupId= 88ca1ad3c453cb5bb8dff79ab9aecaeb' /etc/mongodb-mms/automation-agent.config
 systemctl start mongodb-mms-automation-agent.service
